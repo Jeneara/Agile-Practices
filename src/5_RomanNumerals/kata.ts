1bf0973384ad92576f408c4b719b8b19b350d05e
Steph Mason <stephanie.mason@vista.com>
2023-11-14 21:12:30 +1100

    feat: roman numerals kata

--- src/5_RomanNumerals/kata.ts
+++ src/5_RomanNumerals/kata.ts
@@ -1,1 +1,33 @@
-export const romanNumeral = 'roman numeral';
+const baseNumbers = new Map<number, string>([
+  [1000, 'M'],
+  [900, 'CM'],
+  [500, 'D'],
+  [400, 'CD'],
+  [100, 'C'],
+  [90, 'XC'],
+  [50, 'L'],
+  [40, 'XL'],
+  [10, 'X'],
+  [9, 'IX'],
+  [5, 'V'],
+  [4, 'IV'],
+  [1, 'I'],
+]);
+
+export const romanNumeral = (arabicNumber: number): string | undefined => {
+  // Behaviours
+  // 1. Whole numbers
+  // 2. Subtraction numbers -
+  // 3. Addition numbers +
+
+  let romanNumeral = '';
+
+  baseNumbers.forEach((value, key) => {
+    while (arabicNumber >= key) {
+      arabicNumber -= key;
+      romanNumeral += value;
+    }
+  });
+
+  return romanNumeral;
+};
--- src/5_RomanNumerals/test.ts
+++ src/5_RomanNumerals/test.ts
@@ -1,7 +1,63 @@
 import { romanNumeral } from './kata';
 
 describe('Roman numeral should', () => {
-  test('convert arabic number to roman numeral', () => {
-    expect(romanNumeral).toBe('roman numeral');
+  test('should return I if arabic number is 1', () => {
+    expect(romanNumeral(1)).toBe('I');
+  });
+
+  test('should return V if arabic number is 5', () => {
+    expect(romanNumeral(5)).toBe('V');
+  });
+
+  test('should return X if arabic number is 10', () => {
+    expect(romanNumeral(10)).toBe('X');
+  });
+
+  test('should return L if arabic number is 50', () => {
+    expect(romanNumeral(50)).toBe('L');
+  });
+
+  test('should return C if arabic number is 100', () => {
+    expect(romanNumeral(100)).toBe('C');
+  });
+
+  test('should return D if arabic number is 500', () => {
+    expect(romanNumeral(500)).toBe('D');
+  });
+
+  test('should return XX if arabic number is 1000', () => {
+    expect(romanNumeral(1000)).toBe('M');
+  });
+
+  test('should return II if arabic number is 2', () => {
+    expect(romanNumeral(2)).toBe('II');
+  });
+
+  test('should return III if arabic number is 3', () => {
+    expect(romanNumeral(3)).toBe('III');
+  });
+
+  test('should return VI if arabic number is 6', () => {
+    expect(romanNumeral(6)).toBe('VI');
+  });
+
+  test('should return VII if arabic number is 7', () => {
+    expect(romanNumeral(7)).toBe('VII');
+  });
+
+  test('should return VIII if arabic number is 8', () => {
+    expect(romanNumeral(8)).toBe('VIII');
+  });
+
+  test('should return MMM if arabic number is 3000', () => {
+    expect(romanNumeral(3000)).toBe('MMM');
+  });
+
+  test('should return CMXCIX if arabic number is 999', () => {
+    expect(romanNumeral(999)).toBe('CMXCIX');
+  });
+
+  test('should return CDXLIV if arabic number is 444', () => {
+    expect(romanNumeral(444)).toBe('CDXLIV');
   });
 });
