55397ed6585ec90d541956a9e45edf7de773b52e
Steph Mason <stephanie.mason@vista.com>
2023-11-14 20:43:49 +1100

    feat: whole numbers

--- src/5_RomanNumerals/kata.ts
+++ src/5_RomanNumerals/kata.ts
@@ -1,1 +1,21 @@
-export const romanNumeral = 'roman numeral';
+const baseNumbers = new Map<number, string>([
+  [1000, 'M'],
+  [500, 'D'],
+  [100, 'C'],
+  [50, 'L'],
+  [10, 'X'],
+  [5, 'V'],
+  [1, 'I'],
+]);
+
+export const romanNumeral = (arabicNumber: number): string | undefined => {
+  // Behaviours
+  // 1. Whole numbers
+  // 2. Subtraction numbers -
+  // 3. Addition numbers +
+
+  // can base number be found in arabic number - that symbol is the one we use
+
+  // Basic Numbers 1 & 5 & 10 & 50 & 100 & 500 & 1000
+  return baseNumbers.get(arabicNumber);
+};
--- src/5_RomanNumerals/test.ts
+++ src/5_RomanNumerals/test.ts
@@ -1,7 +1,31 @@
 import { romanNumeral } from './kata';
 
 describe('Roman numeral should', () => {
-  test('convert arabic number to roman numeral', () => {
-    expect(romanNumeral).toBe('roman numeral');
+  test('should return I if arabic number is 1', () => {
+    expect(romanNumeral(1)).toBe('I');
+  });
+
+  test('should return V if arabic number is 5', () => {
+    expect(romanNumeral(5)).toBe('V');
+  });
+
+  test('should return X if arabic number is 10', () => {
+    expect(romanNumeral(10)).toBe('X');
+  });
+
+  test('should return L if arabic number is 50', () => {
+    expect(romanNumeral(50)).toBe('L');
+  });
+
+  test('should return C if arabic number is 100', () => {
+    expect(romanNumeral(100)).toBe('C');
+  });
+
+  test('should return D if arabic number is 500', () => {
+    expect(romanNumeral(500)).toBe('D');
+  });
+
+  test('should return XX if arabic number is 1000', () => {
+    expect(romanNumeral(1000)).toBe('M');
   });
 });
