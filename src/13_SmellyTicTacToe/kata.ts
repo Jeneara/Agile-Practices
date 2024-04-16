bfa71d06e7fd1a8e274887970f800082a3087df2
Steph Mason <stephanie.mason@vista.com>
2024-04-16 17:15:01 +1000

    feat: smelly TicTacToe

--- src/13_SmellyTicTacToe/kata.ts
+++ src/13_SmellyTicTacToe/kata.ts
@@ -4,50 +4,67 @@ export class Game {
   private _lastSymbol = ' ';
   private _board: Board = new Board();
 
+  //Primitive Obsession Code Smell
+  // Data Clumps Code Smell
+  // Long Method Code Smell
   public Play(symbol: string, x: number, y: number): void {
+    // Comments Code Smell
     //if first move
     if (this._lastSymbol == ' ') {
+      // Comments Code Smell
       //if player is X
       if (symbol == 'O') {
         throw new Error('Invalid first player');
       }
     }
+    // Comments Code Smell
+    // Switch Statements Code Smell
     //if not first move but player repeated
     else if (symbol == this._lastSymbol) {
       throw new Error('Invalid next player');
     }
+    // Comments Code Smell
+    // Switch Statements Code Smell
     //if not first move but play on an already played tile
+    // Inappropriate Intimacy Code Smell
     else if (this._board.TileAt(x, y).Symbol != ' ') {
       throw new Error('Invalid position');
     }
-
+    // Comments Code Smell
     // update game state
     this._lastSymbol = symbol;
     this._board.AddTileAt(symbol, x, y);
   }
 
+  // Long Method Code Smell
   public Winner(): string {
+    // Comments Code Smell
     //if the positions in first row are taken
+    // Duplicate Code Code Smell
     if (
+      // Feature Envy Code Smell (on board)
       this._board.TileAt(0, 0)!.Symbol != ' ' &&
       this._board.TileAt(0, 1)!.Symbol != ' ' &&
       this._board.TileAt(0, 2)!.Symbol != ' '
     ) {
+      // Comments Code Smell
       //if first row is full with same symbol
       if (
+        // Message Chains Code Smell
         this._board.TileAt(0, 0)!.Symbol == this._board.TileAt(0, 1)!.Symbol &&
         this._board.TileAt(0, 2)!.Symbol == this._board.TileAt(0, 1)!.Symbol
       ) {
         return this._board.TileAt(0, 0)!.Symbol;
       }
     }
-
+    // Comments Code Smell
     //if the positions in first row are taken
     if (
       this._board.TileAt(1, 0)!.Symbol != ' ' &&
       this._board.TileAt(1, 1)!.Symbol != ' ' &&
       this._board.TileAt(1, 2)!.Symbol != ' '
     ) {
+      // Comments Code Smell
       //if middle row is full with same symbol
       if (
         this._board.TileAt(1, 0)!.Symbol == this._board.TileAt(1, 1)!.Symbol &&
@@ -56,13 +73,14 @@ export class Game {
         return this._board.TileAt(1, 0)!.Symbol;
       }
     }
-
+    // Comments Code Smell
     //if the positions in first row are taken
     if (
       this._board.TileAt(2, 0)!.Symbol != ' ' &&
       this._board.TileAt(2, 1)!.Symbol != ' ' &&
       this._board.TileAt(2, 2)!.Symbol != ' '
     ) {
+      // Comments Code Smell
       //if middle row is full with same symbol
       if (
         this._board.TileAt(2, 0)!.Symbol == this._board.TileAt(2, 1)!.Symbol &&
@@ -77,9 +95,9 @@ export class Game {
 }
 
 interface Tile {
-  X: number;
-  Y: number;
-  Symbol: string;
+  X: number; //Primitive Obsession Code Smell
+  Y: number; //Primitive Obsession Code Smell
+  Symbol: string; //Primitive Obsession Code Smell
 }
 
 class Board {
@@ -95,13 +113,18 @@ class Board {
   }
 
   public TileAt(x: number, y: number): Tile {
+    // Duplicate Code Code Smell
     return this._plays.find((t: Tile) => t.X == x && t.Y == y)!;
   }
 
+  // Data Clump Code Smell
+  // Primitive Obsession Code Smell
+  // Long Parameter List Code Smell
   public AddTileAt(symbol: string, x: number, y: number): void {
     //@ts-ignore
+    // Dead Code Code Smell
     const tile: Tile = { X: x, Y: y, Symbol: symbol };
-
+    // Message Chains Code Smell
     this._plays.find((t: Tile) => t.X == x && t.Y == y)!.Symbol = symbol;
   }
 }
